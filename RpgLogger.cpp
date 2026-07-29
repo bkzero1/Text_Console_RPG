67df83f1699d5c5d10d522de1b620709f179d45f
ohyo <khr157929@gmail.com>
2026-07-29 20:16:21 +0900

    feat: 기능 테스트 추가

--- RpgLogger.cpp
+++ RpgLogger.cpp
@@ -1,23 +1,84 @@
-#include <iostream>
 #include "RpgLogger.h"
 
-void RpgLogger::showLogs()
+#include <iostream>
+
+// 임시 작성
+namespace Test
 {
-    std::cout << "전체 로그 출력" << std::endl;
+enum class EMonsterID
+{
+    NONE,
+    GOBLIN,
+    ORC
+};
+std::map<EMonsterID, std::string> convertMonsterString = {
+    {EMonsterID::NONE, "NONE"}, {EMonsterID::GOBLIN, "GOBLIN"}, {EMonsterID::ORC, "ORK"}};
+}  // namespace Test
+
+void RpgLogger::ShowLogs()
+{
+    std::cout << "------------- Recent Logs -------------" << std::endl;
+
+    auto QueueCopy = logs;
+    while (!QueueCopy.empty())
+    {
+        std::cout << QueueCopy.front() << "\n";
+        QueueCopy.pop();
+    }
+
+    std::cout << "---------------------------------------" << std::endl;
 }
 
-void RpgLogger::showKillLogs()
+void RpgLogger::ShowKillLogs()
 {
-    std::cout << "킬 로그 출력" << std::endl;
+    std::cout << "------------- Monster Kill Count -------------" << std::endl;
+
+    for (auto& [key, value] : killCounts)
+    {
+        std::cout << Test::convertMonsterString.at(key) << "을 " << value << "마리 처치하였습니다." << std::endl;
+    }
+
+    std::cout << "----------------------------------------------" << std::endl;
 }
 
 void RpgLogger::AddLog(std::string log)
 {
-    // 로그 추가
+    logs.push(log);
+
+    if (logs.size() > MAX_SIZE)
+    {
+        logs.pop();
+    }
+
     std::cout << log << std::endl;
 }
 
-void RpgLogger::OnMonsterKilled(EMonsterID id)
+void RpgLogger::OnMonsterKilled(Test::EMonsterID id)
 {
-    std::cout << "몬스터 처치 횟수 증가" << std::endl;
+    killCounts[id]++;
+}
+
+void TestRpgLogger()
+{
+    RpgLogger logger;
+    logger.AddLog("캐릭터를 생성했어요!");
+    logger.AddLog("고블린 몬스터가 등장했어요!");
+    logger.AddLog("플레이어가 몬스터를 공격합니다!");
+    logger.AddLog("몬스터가 플레이어를 공격합니다!");
+    logger.AddLog("플레이어가 몬스터를 공격합니다!");
+    logger.AddLog("몬스터가 플레이어를 공격합니다!");
+    logger.AddLog("플레이어가 몬스터를 공격합니다!");
+    logger.AddLog("플레이어가 몬스터를 처치했습니다~!!");
+    logger.OnMonsterKilled(Test::EMonsterID::GOBLIN);
+
+    logger.ShowLogs();
+
+    logger.ShowKillLogs();
+
+    for (int i = 0; i < 15; i++)
+    {
+        logger.AddLog(std::to_string(i) + "번째 로그");
+    }
+
+    logger.ShowLogs();
 }
