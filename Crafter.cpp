e1a6621641ad39408ffb7957526280238c8fd408
김채우 <codn0185@gmail.com>
2026-07-30 19:26:04 +0900

    헤더에 메서드 정의, TRY_CRAFT_ITEM() 구현

--- Crafter.cpp
+++ Crafter.cpp
@@ -1,1 +1,38 @@
 #include "Crafter.h"
+
+const std::map<EItemID, CraftingRecipe> CRAFTING_RECIPE_TABLE = {
+
+};
+
+bool Crafter::TRY_CRAFT_ITEM(Inventory* inventory, EItemID targetItemID)
+{
+    // 제작 레시피가 존재하지 않음
+    if (CRAFTING_RECIPE_TABLE.find(targetItemID) == CRAFTING_RECIPE_TABLE.end())
+    {
+        return false;
+    }
+
+    // 인벤토리에 제작할 아이템을 추가할 공간 부족
+    if (inventory->GetMaxAddableItemCount(targetItemID) == 0)
+    {
+        return false;
+    }
+
+    // 인벤토리 내 제작 재료 부족
+    for (const auto& [ingredientItemID, ingredientCount] : CRAFTING_RECIPE_TABLE.at(targetItemID).ingredients)
+    {
+        if (inventory->GetItemCount(ingredientItemID) < ingredientCount)
+        {
+            return false;
+        }
+    }
+
+    // 아이템 제작 - 재료 소모 & 아이템 추가
+    for (const auto& [ingredientItemID, ingredientCount] : CRAFTING_RECIPE_TABLE.at(targetItemID).ingredients)
+    {
+        inventory->ConsumeItem(ingredientItemID, ingredientCount);  // 제작 재료 소모
+    }
+    inventory->AddItem(targetItemID);  // 제작한 아이템 추가
+
+    return true;
+}
--- Crafter.h
+++ Crafter.h
@@ -1,4 +1,21 @@
 #pragma once
+
+#include <string>
+#include <vector>
+
+#include "CraftingRecipe.h"
+#include "Inventory.h"
+
+extern const std::map<EItemID, CraftingRecipe> CRAFTING_RECIPE_TABLE;
+
 class Crafter
 {
+   public:
+    static void SHOW_ALL_RECIPES();                                                // 모든 제작 레시피 출력
+    static void SHOW_RECIPES(const std::vector<CraftingRecipe>& craftingRecipes);  // 주어진 제작 레시피 출력
+
+    static bool TRY_CRAFT_ITEM(Inventory* inventory, EItemID targetItemID);  // 아이템 제작 시도 (제작 성공 시 인벤토리 반영 및 true 반환, 제작 실패 시 false 반환)
+
+    static std::vector<CraftingRecipe> SEARCH_RECIPES_BY_ITEM_NAME(const std::string keyword);        // 제작 아이템 이름으로 검색한 레시피들 반환
+    static std::vector<CraftingRecipe> SEARCH_RECIPES_BY_INGREDIENT_NAME(const std::string keyword);  // 제작 재료 이름으로 검색한 레시피들 반환
 };
