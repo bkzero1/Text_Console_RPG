dc34c548bd4b3f3823c5132f31de12e1f47f8ef8
이승현 <lsungsung12@gmail.com>
2026-07-30 21:19:48 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ enum class EGameState
 // 전역 변수
 EGameState CurrentGameState = EGameState::PLAYER_INIT;
 bool IsRunning = true;
-
+std::vector<Player*> Players; // 플레이어 목록
 // 게임 상태 전환
 void SwitchState(EGameState newGameState)
 {
