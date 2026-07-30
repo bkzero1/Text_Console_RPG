7e69d42cfd3a45fe789dff21fe89ef827c159651
JsP <84488611+bkzero1@users.noreply.github.com>
2026-07-30 10:49:09 +0900

    Monster.cpp 기본 반환값 적용

--- Monster.cpp
+++ Monster.cpp
@@ -1,1 +1,44 @@
 ﻿#include "Monster.h"
+
+std::string Monster::Deploy(const FMonsterData &monsterData, int playerLevel)
+{
+    return std::string();
+}
+
+void Monster::ShowStatus() const
+{
+}
+
+void Monster::TakeDamage(int damage)
+{
+}
+
+int Monster::GetPower() const
+{
+    return 0;
+}
+
+bool Monster::IsDead() const
+{
+    return false;
+}
+
+const std::vector<EItemID> &Monster::GetDropItems() const
+{
+    // TODO: 여기에 return 문을 삽입합니다.
+}
+
+const std::string &Monster::GetName() const
+{
+    // TODO: 여기에 return 문을 삽입합니다.
+}
+
+int Monster::GetGold() const
+{
+    return 0;
+}
+
+int Monster::GetExp() const
+{
+    return 0;
+}
