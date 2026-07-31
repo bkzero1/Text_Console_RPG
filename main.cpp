8e38c4918b83c6c8c1b64b5cedecc6bc33521dd8
이승기 <thouxcv@gmail.com>
2026-07-31 12:29:33 +0900

    추가, 로그, 전투 후 아이템을 몇개 얻었는지 로그 출력 추가

--- main.cpp
+++ main.cpp
@@ -234,10 +234,18 @@ void NormalBattle()
     }
 
     // 전리품 인벤토리에 추가
+    // 골드 획득
     inventory->AddGold(battleManager.GetEarnGold());    
     rpgLogger.AddLog("파티는 " + to_string(battleManager.GetEarnGold()) + "골드를 얻었다");
-    // 골드 획득
-    std::map<EItemID, int> remainingItems = inventory->AddItems(battleManager.GetEarnItems());  // 아이템 획득
+    std::map<EItemID, int> earnItems = battleManager.GetEarnItems();
+    for (auto itr = earnItems.begin(); itr != earnItems.end(); itr++)
+    {
+        std::string earnItemName = ITEM_TABLE.at(itr->first).name;
+        int earnItemNumber = itr->second;
+        rpgLogger.AddLog(earnItemName + " " + to_string(earnItemNumber) + "개 발견");
+    }
+
+    std::map<EItemID, int> remainingItems = inventory->AddItems(earnItems);  // 아이템 획득
     while (!remainingItems.empty())
     {
         inventory->ShowInventory();
