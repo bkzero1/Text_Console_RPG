6b8e915f235d444527ee75ecc23def0d1669682d
김채우 <codn0185@gmail.com>
2026-07-31 17:30:27 +0900

    CRAFTING_RECIPE_TABLE 내 임시 데이터 수정

--- CraftingRecipe.cpp
+++ CraftingRecipe.cpp
@@ -8,7 +8,6 @@ CraftingRecipe::CraftingRecipe(EItemID itemID, std::map<EItemID, int> ingredient
 const std::map<EItemID, CraftingRecipe> CRAFTING_RECIPE_TABLE = {
     {EItemID::HP_POTION, CraftingRecipe(EItemID::HP_POTION,  // 임시 데이터
                                         {
-                                            {EItemID::NONE, 1},
                                             {EItemID::HP_POTION, 2},
                                             {EItemID::POWER_POTION, 3},
                                         })},
