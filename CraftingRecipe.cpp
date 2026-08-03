7c916c2f8934bc4c8749a11a090b9324efb87598
김채우 <codn0185@gmail.com>
2026-08-03 15:23:36 +0900

    refactor: 레시피 관리 시스템 수정
    - 기존의 아이템 하나가 최대 1개의 레시피를 가지는 것을 수정하여 한 아이템에 복수의 레시피를 등록하도록 수정
    - 필터 추가/클리어 시 자동으로 적용되는 로직에서 필터 적용 메서드를 분리함

--- CraftingRecipe.cpp
+++ CraftingRecipe.cpp
@@ -5,19 +5,29 @@ CraftingRecipe::CraftingRecipe(EItemID itemID, std::map<EItemID, int> ingredient
 {
 }
 
-const std::map<EItemID, CraftingRecipe> CRAFTING_RECIPE_TABLE = {
+const std::map<EItemID, std::vector<CraftingRecipe>> CRAFTING_RECIPE_TABLE = {
     // HP 포션
-    {EItemID::HP_POTION, CraftingRecipe(EItemID::HP_POTION,
-                                        {
-                                            {EItemID::BOTTLE, 1},
-                                            {EItemID::WATER, 2},
-                                            {EItemID::SLIME_JELLY, 1},
-                                        })},
+    {
+        EItemID::HP_POTION,
+        {
+            CraftingRecipe(EItemID::HP_POTION, {}),  // 임시 레피시 (재료 없이 제작 가능)
+            CraftingRecipe(EItemID::HP_POTION, {
+                                                   {EItemID::BOTTLE, 1},
+                                                   {EItemID::WATER, 2},
+                                                   {EItemID::SLIME_JELLY, 1},
+                                               }),
+        },
+    },
     // 공격력 증가 포션
-    {EItemID::POWER_POTION, CraftingRecipe(EItemID::POWER_POTION,
-                                           {
-                                               {EItemID::BOTTLE, 1},
-                                               {EItemID::SLIME_CORE, 1},
-                                               {EItemID::SLIME_CLEAR_WATER_DROP, 1},
-                                           })},
+    {
+        EItemID::POWER_POTION,
+        {
+            CraftingRecipe(EItemID::POWER_POTION, {}),  // 임시 레피시 (재료 없이 제작 가능)
+            CraftingRecipe(EItemID::POWER_POTION, {
+                                                      {EItemID::BOTTLE, 1},
+                                                      {EItemID::SLIME_CORE, 1},
+                                                      {EItemID::SLIME_CLEAR_WATER_DROP, 1},
+                                                  }),
+        },
+    },
 };
