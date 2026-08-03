8a8ef7ec63a761eaec979816132ffa50a629bfd8
이승기 <thouxcv@gmail.com>
2026-08-03 16:47:31 +0900

    수정, 디버그, 배틀 매니저 초기화 되지 않던 문제 수정

--- main.cpp
+++ main.cpp
@@ -198,7 +198,7 @@ bool BattlePhase(BattleManager& battleManager, MonsterPool& monsterPool)
 // 일반 전투
 void NormalBattle()
 {
-    BattleManager battleManager = BattleManager();
+    BattleManager battleManager;
     MonsterPool monsterPool = MonsterPool();
 
     int totalLv = 0;
