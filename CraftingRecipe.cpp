f1912963998e0a9b39ad42cd2e943d907cc6369a
김채우 <codn0185@gmail.com>
2026-08-03 11:41:56 +0900

    제작 레시피 추가 - HP_POTION - POWER_POTION

--- CraftingRecipe.cpp
+++ CraftingRecipe.cpp
@@ -6,9 +6,18 @@ CraftingRecipe::CraftingRecipe(EItemID itemID, std::map<EItemID, int> ingredient
 }
 
 const std::map<EItemID, CraftingRecipe> CRAFTING_RECIPE_TABLE = {
-    {EItemID::HP_POTION, CraftingRecipe(EItemID::HP_POTION,  // 임시 데이터
+    // HP 포션
+    {EItemID::HP_POTION, CraftingRecipe(EItemID::HP_POTION,
                                         {
-                                            {EItemID::HP_POTION, 2},
-                                            {EItemID::POWER_POTION, 3},
+                                            {EItemID::BOTTLE, 1},
+                                            {EItemID::WATER, 2},
+                                            {EItemID::SLIME_JELLY, 1},
                                         })},
+    // 공격력 증가 포션
+    {EItemID::POWER_POTION, CraftingRecipe(EItemID::POWER_POTION,
+                                           {
+                                               {EItemID::BOTTLE, 1},
+                                               {EItemID::SLIME_CORE, 1},
+                                               {EItemID::SLIME_CLEAR_WATER_DROP, 1},
+                                           })},
 };
